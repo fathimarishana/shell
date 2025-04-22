d49f30d31edf5bf6fd2d2a20e3297dacfbbbb4ef
fathimarishana <fathimarishana835@gmail.com>
2025-04-22 12:24:56 +0530

    Delete exp10.py

--- exp10.py
+++
@@ -1,7 +0,0 @@
-color_list1 = input("enter colors for color_list1 (seperated by spaces):").split()
-color_list2 = input("enter colors for color_list2 (seperated by spaces):").split() 
-colors_not_in_list2 =[]
-for color in color_list1:
-    if color not in color_list2:
-        colors_not_in_list2.append(color)
-print("colors from color_list1 not contained in color_list2",colors_not_in_list2)
