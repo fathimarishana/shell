56127c12c89435bb7541bc466fe303400446b939
fathimarishana <fathimarishana835@gmail.com>
2025-04-22 12:26:02 +0530

    Delete exp7.py

--- exp7.py
+++
@@ -1,9 +0,0 @@
-text=input("enter a line of lext:")
-words=text.split()
-words_count={}
-for word in words:
-    if word in words_count:
-       words_count[word]+=1
-    else:
-        words_count[word]=1
-print(words_count)
